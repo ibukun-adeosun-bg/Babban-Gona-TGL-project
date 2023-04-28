f0f39ad2b98f432bf125c7dd05f644559d8a4703
Ibukunoluwa Adeosun <Ibadeosun@gmail.com>
2023-04-28 11:14:40 +0100

    implemented the uploading file feature

--- routes/operatorRoutes.js
+++ routes/operatorRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const multer = require("multer")
+const upload = multer({ dest: 'uploads/' });
+const bodyParser = require("body-parser")
 const { verifyUser, verifyAdmin } = require("../middleware/verifyToken")
 const { createOperator, getOperator, getAllOperators, updateOperator, deleteOperator } = require("../controllers/operatorController")
 const router = express.Router()
 
 //CREATE AN OPERATOR
-router.post("/:username/operators", verifyUser, createOperator)
+router.post("/:username/operators", upload.single("file"), bodyParser.urlencoded({ extended: true }), verifyUser, createOperator)
 
 //GET AN OPERATOR
 router.get("/:username/operators/:operatorId", verifyUser || verifyAdmin, getOperator)
@@ -13,7 +16,7 @@ router.get("/:username/operators/:operatorId", verifyUser || verifyAdmin, getOpe
 router.get("/:username/operators", verifyAdmin, getAllOperators)
 
 //UPDATE OPERATOR INFORMATION
-router.put("/:username/operators/:operatorId", verifyUser || verifyAdmin, updateOperator)
+router.put("/:username/operators/:operatorId", upload.single("file"), bodyParser.urlencoded({ extended: true }), verifyUser || verifyAdmin, updateOperator)
 
 //DELETE OPERATOR INFORMATION
 router.delete("/:username/operators/:operatorId", verifyUser || verifyAdmin, deleteOperator)
